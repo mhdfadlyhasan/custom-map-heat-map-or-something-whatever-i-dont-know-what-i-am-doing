d1f88d20b5f916a6bcee1fcccae162009b2e52f2
Mhd.Fadly Hasan <mhdfadlyhasan16a1@gmail.com>
2023-04-15 13:35:56 +0700

    forgot to disable the red circle

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ var map = L.map("map").setView(
   document.getElementById("default-coordinate").value.split(","),
   13
 );
-var circle = L.circle([-7.282103, 112.747903], {
-  color: "red",
-  fillColor: "#f03",
-  fillOpacity: 0.2,
-  radius: 3000,
-}).addTo(map);
+// var circle = L.circle([-7.282103, 112.747903], {
+//   color: "red",
+//   fillColor: "#f03",
+//   fillOpacity: 0.2,
+//   radius: 3000,
+// }).addTo(map);
 
 var tiles = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
